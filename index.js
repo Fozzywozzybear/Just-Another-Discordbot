9edfd832b92cf04d5b8741afff42aa353b65dfc8
Fozzywozzybear <54587293+Fozzywozzybear@users.noreply.github.com>
2020-03-27 11:08:44 -0400

    Updated index.js
    Added two api functions to the bot, need to learn how to do api for covid api

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const Discord = require('discord.js');
 const { prefix,token } = require('./config.json');
 const client = new Discord.Client();
+const fetch = require('node-fetch');
+const trim = (str, max) => ((str.length > max) ? `${str.slice(0, max - 3)}...` : str);
+const querystring = require('querystring');
 
 
 client.once('ready',() => {
     console.log('Ready to go')
 })
 
-client.on('message',message => {
+/*client.on('message',message => {
     //console.log(message.content);
     //!test will run the bot to prompt with This is working 
     if(message.content.startsWith(`${prefix}test`)){
@@ -16,5 +19,47 @@ client.on('message',message => {
         message.channel.send(`This server's name is: ${message.guild.name}`)
     }
 })
+*/
+
+client.on('message', async message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if (command === 'cat') {
+        const { file } = await fetch('https://aws.random.cat/meow').then(response => response.json());
+    
+        message.channel.send(file);
+    } else if (command === 'urban') {
+        if (!args.length) {
+            return message.channel.send('You need to supply a search term!');
+        }
+
+        const query = querystring.stringify({ term: args.join(' ') });
+        
+        const { list } = await fetch(`https://api.urbandictionary.com/v0/define?${query}`).then(response => response.json());
+        
+        if (!list.length) {
+            return message.channel.send(`No results found for **${args.join(' ')}**.`);
+        }
+        const [answer] = list;
+
+        const embed = new Discord.MessageEmbed()
+            .setColor('#EFFF00')
+            .setTitle(answer.word)
+            .setURL(answer.permalink)
+            .addFields(
+            { name: 'Definition', value: trim(answer.definition, 1024) },
+            { name: 'Example', value: trim(answer.example, 1024) },
+            { name: 'Rating', value: `${answer.thumbs_up} thumbs up. ${answer.thumbs_down} thumbs down.` }
+            );
+
+        message.channel.send(embed);
+
+        }
+});
+
+
 
 client.login(token);
