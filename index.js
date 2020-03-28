b1b651e7ffb165b1f25325b2db3202eb155582cf
Fozzywozzybear <54587293+Fozzywozzybear@users.noreply.github.com>
2020-03-28 14:15:40 -0400

    Music added
    The feature to add music is now working for matt

--- index.js
+++ index.js
@@ -6,11 +6,19 @@ const fetch = require('node-fetch');
 const trim = (str, max) => ((str.length > max) ? `${str.slice(0, max - 3)}...` : str);
 const querystring = require('querystring');
 const ytdl = require('ytdl-core');
-var servers = {};
+const queue=new Map()
 
+
+// Logs to see what the bot is doing 
 client.once('ready',() => {
     console.log('Ready to go')
 })
+client.once('reconnecting', () => {
+    console.log('Reconnecting!');
+});
+client.once('disconnect', () => {
+    console.log('Disconnect!');
+});
 
 /*client.on('message',message => {
     //console.log(message.content);
@@ -26,56 +34,75 @@ client.once('ready',() => {
 client.on('message', async message => {
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-	//const args = message.content.slice(prefix.length).split(" ");
-    //const command = args.shift().toLowerCase();
-    let args = message.content.substring(prefix.length).split(" ");
+	const args = message.content.slice(prefix.length).split(" ");
+    const command = args.shift().toLowerCase();
+    const serverQueue = queue.get(message.guild.id);
     
-    switch (args[0]){
-        case 'play':
-            function play(connection, message){
-                var server = servers[message.guild.id];
-                
-                server.dispatcher = connection.play(ytdl(server.queue[0], {filter: "audioonly"}));
-
-                server.queue.shift();
-
-                server.dispatcher.on("end",function(){
-                    if (server.queue[0]){
-                        play(connection,message);
-                    }else {
-                        connection.disconnect();
-                    }
-                    
-                });
-            
-            
-            }
-
-            if (!args[1]){
-                message.channel.send("You need a link to the song");
-                return;
-            }
-            if(!message.member.voice.channel){
-                message.channel.send("You must be in a channel to play a song")
-                return;
-            }
-            if(!servers[message.guild.id]) servers[message.guild.id] = {
-                queue: []
-            }
-
-            var server = servers[message.guild.id];
-
-            server.queue.push(args[1]);
-
-            if(!message.guild.voiceConnection) message.member.voice.channel.join().then(function(connection){
-                play(connection, message);
-            })
-
-
-
-        break;     
-
+    if (message.content.startsWith(`${prefix}play`)){
+        execute(message, serverQueue);
+        return;
     }
+    else if (message.content.startsWith(`${prefix}skip`)){
+        skip(message, serverQueue);
+        return;
+    }
+    else if (message.content.startsWith(`${prefix}stop`)){
+        stop(message, serverQueue);
+        return;
+    } else{
+        message.channel.send("you need to enter a valid command you clown")
+    }
+    // let args = message.content.substring(prefix.length).split(" ");
+    
+    // switch (args[0]){
+    //     case 'play':
+    //         function play(connection, message){
+    //             var server = servers[message.guild.id];
+                
+    //             server.dispatcher = connection.play(ytdl(server.queue[0], {filter: "audioonly"}));
+
+    //             server.queue.shift();
+
+    //             server.dispatcher.on("end",function(){
+    //                 if (server.queue[0]){
+    //                     play(connection,message);
+    //                 }else {
+    //                     connection.disconnect();
+    //                 }
+                    
+    //             });
+            
+            
+    //         }
+
+    //         if (!args[1]){
+    //             message.channel.send("You need a link to the song");
+    //             return;
+    //         }
+    //         if(!message.member.voice.channel){
+    //             message.channel.send("You must be in a channel to play a song")
+    //             return;
+    //         }
+    //         if(!servers[message.guild.id]) servers[message.guild.id] = {
+    //             queue: []
+    //         }
+
+    //         var server = servers[message.guild.id];
+
+    //         server.queue.push(args[1]);
+
+    //         if(!message.guild.voiceConnection) message.member.voice.channel.join().then(function(connection){
+    //             play(connection, message);
+    //         })
+
+
+
+    //     break;     
+
+    // }
+
+    //Start of commands for music section of bot.
+    
 
 	if (command === 'cat') {
         const { file } = await fetch('https://aws.random.cat/meow').then(response => response.json());
@@ -121,6 +148,99 @@ client.on('message', async message => {
        
 
 });
+async function execute(message, serverQueue){
+    const args = message.content.split(" ");
+    
+    const voiceChannel = message.member.voice.channel
+    if (!voiceChannel)
+    return message.channel.send(
+        "You need to be in a voice chanel to play music!"
+    );
+    const permissions = voiceChannel.permissionsFor(message.client.user);
+    if (!permissions.has("CONNECT") || !permissions.has("SPEAK")){
+        return message.channel.send(
+            "I need the tape off my mouth so that I can talk."
+        );
+    }
+    const songInfo = await ytdl.getInfo(args[1]);
+    const song = {
+        title: songInfo.title,
+        url: songInfo.video_url,
+};
+
+
+
+if (!serverQueue){
+    const queueContruct = {
+        textChannel: message.channel,
+        voiceChannel: voiceChannel,
+        connection: null,
+        songs: [],
+        volume: 5,
+        playing: true
+      };
+    // Setting the queue using our contract
+   queue.set(message.guild.id, queueContruct);
+   // Pushing the song to our songs array
+   queueContruct.songs.push(song);
+   
+   try {
+    // Here we try to join the voicechat and save our connection into our object.
+    var connection = await voiceChannel.join();
+    queueContruct.connection = connection;
+    // Calling the play function to start a song
+    play(message.guild, queueContruct.songs[0]);
+   } catch (err) {
+    // Printing the error message if the bot fails to join the voicechat
+    console.log(err);
+    queue.delete(message.guild.id);
+    return message.channel.send(err);
+
+}
+} else {
+    serverQueue.songs.push(song);
+    console.log(serverQueue.songs);
+    return message.channel.send(`${song.title} has been added to the queue!`); 
+}
+
+function skip(message, serverQueue) {
+    if (!message.member.voice.channel)
+    return message.channel.send(
+        "You have to be in a voice channel to stop the music!"
+    );
+    if (!serverQueue)
+    return message.channel.send("There is no song that I could skip!");
+    serverQueue.connection.dispatcher.end();
+}
+function stop(message, serverQueue) {
+    if (!message.member.voice.channel)
+      return message.channel.send(
+        "You have to be in a voice channel to stop the music!"
+      );
+    serverQueue.songs = [];
+    serverQueue.connection.dispatcher.end();
+  }
+  function play(guild, song){
+    const serverQueue = queue.get(guild.id);
+    if (!song){
+        serverQueue.voice.channel.leave();
+        queue.delete(guild.id);
+        return;
+    }
+    const dispatcher = serverQueue.connection
+    .play(ytdl(song.url))
+    .on("finish", () => {
+      serverQueue.songs.shift();
+      play(guild, serverQueue.songs[0]);
+    })
+    .on("error", error => console.error(error));
+  dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+  serverQueue.textChannel.send(`Start playing: **${song.title}**`);
+}}
+
+
+
+   
 
 
 
